113b3b2f41a93c4127d549e5b306c52a5d743550
thientm27 <denkhotieu1@gmail.com>
2023-09-21 17:06:58 +0700

    Register with validation email

---
+++ TranMinhThien_Net1606_A01.sln/CarRenting.Client/Pages/Register.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CarRenting.BusinessObjects.Models;
+using CarRenting.DTOs;
+using CarRenting.Repositories.Context;
+
+namespace CarRenting.Client.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly HttpClient _client;
+        private string _apiUrl;
+
+        public RegisterModel()
+        {
+            _client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            _client.DefaultRequestHeaders.Accept.Add(contentType);
+            _apiUrl = Constants.ApiCustomer;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty] public CustomerDto Customer { get; set; } = default!;
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var result = await _client.PostAsJsonAsync(_apiUrl, Customer);
+
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                var dataResponse = result.Content.ReadFromJsonAsync<CustomerDto>().Result;
+                if (dataResponse == null)
+                {
+                    ModelState.AddModelError(Customer.Email, "Duplicated email");
+                }
+            }else if (result.StatusCode == HttpStatusCode.NoContent)
+            {
+                ModelState.AddModelError("Customer.Email", "Duplicated email");
+                return Page();
+            }
+            return RedirectToPage("./Login");
+        }
+    }
+}
--- TranMinhThien_Net1606_A01.sln/CarRenting.DTOs/CustomerDto.cs
+++ TranMinhThien_Net1606_A01.sln/CarRenting.DTOs/CustomerDto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CarRenting.DTOs;
 
 public class CustomerDto
@@ -6,7 +8,7 @@ public class CustomerDto
     public string? CustomerName { get; set; }
     public string? Telephone { get; set; }
     public string Email { get; set; } = null!;
-    public DateTime? CustomerBirthday { get; set; }
+    [DataType(DataType.Date)] public DateTime? CustomerBirthday { get; set; }
     public byte? CustomerStatus { get; set; }
     public string? Password { get; set; }
 }
