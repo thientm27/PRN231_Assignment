7ba9d2150b13210f90fb5e7e0c0b6a6948e3c66b
thientm27 <denkhotieu1@gmail.com>
2023-10-09 09:50:46 +0700

    Car repo

---
+++ TranMinhThien_Net1606_A02.sln/CarRentingOData.Repositories/ICarRepo.cs
@@ -0,0 +1,19 @@
+﻿using CarRenting.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentingOData.Repositories
+{
+    public interface ICarRepo
+    {
+        public Task<List<CarDto>> GetAsync();
+        public Task<List<CarDto>> GetCarAvailable();
+        public Task<CarDto?> AddAsync(CarDto dataDto);
+        public Task<CarDto?> GetByIdAsync(int id);
+        public Task<CarDto?> UpdateAsync(CarDto dataDto);
+        public Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ TranMinhThien_Net1606_A02.sln/CarRentingOData.Repositories/Repo/CarRepo.cs
@@ -0,0 +1,92 @@
+using AutoMapper;
+using CarRenting.DTOs;
+using CarRentingOData.BOs;
+using CarRentingOData.DTOs;
+using CarRentingOData.Repositories;
+using Microsoft.EntityFrameworkCore;
+namespace CarRenting.Repositories.Repo;
+
+public class CarRepo : ICarRepo
+{
+    private readonly CarRentingDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CarRepo()
+    {
+        _context = new CarRentingDbContext();
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<CarDto, Car>();
+            cfg.CreateMap<Car, CarDto>();
+            cfg.CreateMap<CarProducer, CarProducerDto>();
+            cfg.CreateMap<CarProducerDto, CarProducer>();
+        });
+
+        _mapper = new Mapper(config);
+    }
+
+    public async Task<List<CarDto>> GetAsync()
+    {
+        var entities = await _context.Cars
+            .Include(o => o.CarProducer)
+            .ToListAsync();
+        return entities.Select(dto => _mapper.Map<CarDto>(dto)).ToList();
+    }
+    public async Task<List<CarDto>> GetCarAvailable()
+    {
+        var entities = await _context.Cars
+            .Include(o => o.CarProducer)
+            .Where(o => o.Status != "0")
+            .ToListAsync();
+        return entities.Select(dto => _mapper.Map<CarDto>(dto)).ToList();
+    }
+    public async Task<CarDto?> AddAsync(CarDto dataDto)
+    {
+        var entity = _mapper.Map<Car>(dataDto);
+        // var maxId = await _context.CarInformations.MaxAsync(o => o.CarId);
+        // entity.CarId = maxId + 1;
+        var rEntry = await _context.Cars.AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return _mapper.Map<CarDto>(rEntry.Entity);
+    }
+
+    public async Task<CarDto?> GetByIdAsync(int id)
+    {
+        var entity = await _context.Cars
+            .Include(o => o.CarProducer)
+            .FirstOrDefaultAsync(od => od.CarID == id);
+        if (entity == null)
+        {
+            return null;
+        }
+
+        return _mapper.Map<CarDto>(entity);
+    }
+
+    public async Task<CarDto?> UpdateAsync(CarDto dataDto)
+    {
+        var entity = _context.Cars.FirstOrDefault(od => od.CarID == dataDto.CarID);
+        if (entity != null)
+        {
+            _context.Entry(entity).CurrentValues.SetValues(_mapper.Map<Car>(dataDto));
+            await _context.SaveChangesAsync();
+            return _mapper.Map<CarDto>(entity);
+        }
+
+        return null;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var carInformation = await _context.Cars.FirstOrDefaultAsync(od => od.CarID == id);
+        if (carInformation != null)
+        {
+
+            carInformation.Status = "0";
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        return false;
+    }
+}
