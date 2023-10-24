37067f3df18c17426eaf65a51840b46876a80a91
thientm27 <denkhotieu1@gmail.com>
2023-10-24 20:02:24 +0700

    add empty page

---
+++ TranMinhThien_Net1606_A03/CarRenting.Client/Pages/Admin2/CarInformation/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarRenting.Client.Pages.Admin2.CarInformation
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
