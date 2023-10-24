10d5cc21c127dab8378737d9878c000141c60a0f
thientm27 <denkhotieu1@gmail.com>
2023-10-25 00:45:07 +0700

    get available car

---
+++ TranMinhThien_Net1606_A03/CarRenting.Client/Pages/Customer2/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarRenting.Client.Pages.Customer2
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
