595161e9aa04a828c1c6b562fbf491b47e014bd5
thientm27 <denkhotieu1@gmail.com>
2023-10-24 15:18:16 +0700

    add emty edit and delete

---
+++ TranMinhThien_Net1606_A03/CarRenting.Client/Pages/Admin2/Customer/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarRenting.Client.Pages.Admin2.Customer
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
